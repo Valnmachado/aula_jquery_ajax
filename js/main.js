e9988109df4e0d65d190ef85b75d963ce6041265
Valdemir Machado <valdemir.nmachado@gmail.com>
2020-06-29 23:51:51 -0300

    Atualização da aula de bootstrap e JQuery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 function consultaCep(){
+
+    $(".barra-progresso").show();
+
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/"+ cep +"/json/"
 
@@ -12,14 +15,29 @@ function consultaCep(){
             console.log(response);
             
             $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+
+           $(".cep").show();
+           
+           $(".barra-progresso").hide();
+
+            // $("#logradouro").html(response.logradouro);
 
             //document.getElementById("logradouro").innerHTML = response.logradouro;
             
-            document.getElementById("Bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            //document.getElementById("Bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
             
             // alert(response.logradouro);
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
